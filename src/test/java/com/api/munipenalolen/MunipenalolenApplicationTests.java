f5c5b59bbe5f7b440f2e73283305292e3f7e4af8
camidesarrollo <101568254+camidesarrollo@users.noreply.github.com>
2022-09-02 16:36:14 -0400

    First

---
+++ src/test/java/com/api/munipenalolen/MunipenalolenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.munipenalolen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunipenalolenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
